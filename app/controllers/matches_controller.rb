a45bc132808ac174a56c461d14606badfcf03afe
Zach Krzyzanowski <zkrzyz@gmail.com>
2015-11-18 03:30:06 -0500

    Added logic to create relationship record and redirect to relationship if it exists

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,14 +1,21 @@
 class MatchesController < ApplicationController
 	def index
-		@other_users = []
-		@matched_users = []
-		
-		@other_profiles = Profile.where(gender: current_user.profile.looking_for)
-		@other_profiles.each do |u|
-			@other_users << u.user
-		end
+		r = Relationship.arel_table
+		@relationships_active = Relationship.where(r[:user_id].eq(current_user.id).or(r[:ottr_id].eq(current_user.id)).and(r[:status].eq(true))).first
 
-		@current_matches = Match.where(user_id: current_user.id)
+		if @relationships_active
+			redirect_to relationships_path(@relationships_active.id)
+		else
+
+			@other_users = []
+			@matched_users = []
+
+			@other_profiles = Profile.where(gender: current_user.profile.looking_for)
+			@other_profiles.each do |u|
+				@other_users << u.user
+			end
+
+			@current_matches = Match.where(user_id: current_user.id)
 		# @current_matchees = Match.where(matchee_id: current_user.id)
 
 		@current_matches.each do |m|
@@ -21,26 +28,33 @@ class MatchesController < ApplicationController
 		# end
 
 		@other_users = @other_users - @matched_users
+	end
+end
 
+def set_like
+	@all_matches = Match.all
+	@new_match = Match.new
+
+	@new_match.user_id = current_user.id
+	@new_match.matchee_id = params[:id]
+
+	@all_matches.each do |m|
+		puts 'user ' + m.user_id.to_s
+		puts 'matchee ' + m.matchee_id.to_s
+		if m.user_id == @new_match.matchee_id && m.matchee_id == @new_match.user_id
+			Relationship.create(user_id: m.user_id, ottr_id: m.matchee_id, user_flag: 0, status: true)
+		else
+			@new_match.save
+		end
 	end
 
-	def set_like
-		@new_match = Match.new
-		@new_match.user_id = current_user.id
-		@new_match.matchee_id = params[:id]
+end
 
-		@new_match.save
+def set_dislike
+	@new_match = Match.new
+	@new_match.user_id = current_user.id
+	@new_match.nonmatchee_id = params[:id]
 
-		render 'index'
-	end
-
-	def set_dislike
-		@new_match = Match.new
-		@new_match.user_id = current_user.id
-		@new_match.nonmatchee_id = params[:id]
-
-		@new_match.save
-
-		render 'index'
-	end
+	@new_match.save
+end
 end
