882903d565da1101723b90c430145bb6fa5c668d
Zach Krzyzanowski <zkrzyz@gmail.com>
2015-12-30 16:13:15 -0500

    Refactored profile controller and views to use current_user rather than current_profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,16 @@
 class ProfilesController < ApplicationController
 	def show
 		@current_user = User.find(current_user.id)
-		@current_profile = Profile.where(user_id: current_user.id).first
 	end
 
 	def edit
 		@current_user = User.find(current_user.id)
-		@current_profile = Profile.where(user_id: current_user.id).first
 	end
 
 	def update
 		@current_user = User.find(current_user.id)
-		@current_profile = Profile.where(user_id: current_user.id).first
-		if @current_profile.update(profile_params) && @current_user.update(user_params)
+		
+		if @current_user.update(user_params)
 			flash[:alert] = "Your settings were updated successfully."
 			redirect_to profile_path(@current_user)
 		else
@@ -26,9 +24,6 @@ class ProfilesController < ApplicationController
 	def profile_params
 		allow = [:first_name, :last_name, :bio, :gender, :looking_for, :birthday, user_attributes: [:id, :image]]
 		profile_params = params.require(:profile).permit(allow)
-
-		# profile_params.user.delete(:password) unless profile_params.user[:password].present?
-		# profile_params.user.delete(:password_confirmation) unless profile_params.user[:password_confirmation].present?
 
 		return profile_params
 
